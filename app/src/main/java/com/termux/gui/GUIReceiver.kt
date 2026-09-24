1669f63a4e690f2acf7bb19e1f43a91efbaee412
tempuserr <tempuserr@users.noreply.github.com>
2026-09-24 19:06:48 +0200

    Use startForegroundService in GUIReceiver
    Updated the GUIReceiver to use ContextCompat for starting the foreground service, ensuring compatibility with newer Android versions.

--- app/src/main/java/com/termux/gui/GUIReceiver.kt
+++ app/src/main/java/com/termux/gui/GUIReceiver.kt
@@ -6,9 +6,10 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.util.Log
+import androidx.core.content.ContextCompat
 
 /**
- * Receives broadcasts from clients with the address of the main and event socket, adds the requests to the queue and starts the foreground service if necessary.
+ * Receives broadcasts from local GUI clients with the address of the main and event socket, adds the requests to the queue and starts the foreground service if necessary.
  */
 class GUIReceiver : BroadcastReceiver() {
     companion object {
@@ -21,7 +22,7 @@ class GUIReceiver : BroadcastReceiver() {
             Logger.log(1, TAG, "received")
             GUIService.requests.add(GUIService.ConnectionRequest(intent.getStringExtra("mainSocket"),
                     intent.getStringExtra("eventSocket")))
-            context.startService(Intent(context, GUIService::class.java))
+            ContextCompat.startForegroundService(context, Intent(context, GUIService::class.java))
         } catch (e: Exception) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && e is ForegroundServiceStartNotAllowedException) {
                 Log.e(TAG, "could not start service due to foreground service restriction")
